3250eb64e662b05c85e8f9b1c14e32dbf2bfd1ed
LeNico44 <nicolas.houisse@epsi.fr>
2017-12-08 10:14:51 +0100

    correction du fichier calc.js pour passer Jenkins au vert !

--- src/calc.js
+++ src/calc.js
@@ -14,7 +14,7 @@ function calculatrice(signe, gauche, droite) {
     if (typeof gauche === 'number' && typeof droite === 'number') {
            if (gauche>=0 && droite<430 && droite!=0) {
             switch (signe) {
-                /*case "*":
+                case "*":
                     return gauche*droite;
                 case "+":
                     return gauche+droite;
@@ -23,7 +23,7 @@ function calculatrice(signe, gauche, droite) {
                 case "/":
                     return gauche/droite;
                 default:
-                    return null;*/
+                    return null;
             } 
         } else {
                return null;
